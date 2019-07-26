44e2dddd86612f4569298debb4f8f5cd2919f9d3
Andrey Ryzhkov <aryzhkov@aquivalabs.com>
2019-07-26 14:29:21 +0300

    test

--- tour/e2e/protractor.conf.js
+++ tour/e2e/protractor.conf.js
@@ -15,7 +15,7 @@ exports.config = {
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
-    showColors: true,
+    showColors: false,
     defaultTimeoutInterval: 30000,
     print: function() {}
   },
